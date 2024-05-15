cc26fd484ec3f65c0b55ed6c048dbfd951c89423
mclacore <mclacore@gmail.com>
2024-05-15 15:49:19 -0700

    one last test

--- scripts/update-release-notes.py
+++ scripts/update-release-notes.py
@@ -51,7 +51,7 @@ def update_mdx_file(filename, release_notes):
 
 if __name__ == "__main__":
     owner = "massdriver-cloud"
-    repo = "azure-aks-cluster"
+    repo = "azure-postgresql-flexible-server"
     releases = get_latest_releases(owner, repo)
     release_notes = generate_release_notes(releases)
     update_mdx_file("operator.mdx", release_notes)
